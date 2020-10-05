5bdefc4396f1cbffae9c415b5dddce0281441c1b
adispring <sunnyadi@163.com>
2020-10-05 10:28:45 +0800

    动态参数

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,16 @@
 <template>
   <div id="app">
-    <TodoList />
-    <NavigationLink url="https://www.google.com"
-      >Your Profile: {{ user.name }}</NavigationLink
-    >
-    <MixinDemo></MixinDemo>
-    <SlotDemo>
-      <template v-slot:header>
-        <h1>这是 header！</h1>
-      </template>
-      <template>这是 default slot</template>
-      <template v-slot:footer>这是页脚</template>
-    </SlotDemo>
-    <SubmitButton>Save</SubmitButton>
-    <CurrentUser>
-      <template v-slot:default="a">
-        {{ a.user1.firstName }}
-      </template>
-    </CurrentUser>
-    <ScopeSlot>
-      <template v-slot="slotProps">
-        {{ slotProps.student.id }}
-      </template>
-      <template v-slot:teacher="slotProps">
-        {{ slotProps.t.id }}
-      </template>
-    </ScopeSlot>
-    <ScopeSlot v-slot="slotProps">
-      {{ slotProps.student.gender }}
-    </ScopeSlot>
+    <DynamicParam></DynamicParam>
   </div>
 </template>
 
 <script type="text/babel">
-import TodoList from './components/TodoList.vue';
-import NavigationLink from './components/NavigationLink.vue';
-import MixinDemo from './components/MixinDemo.vue';
-import SlotDemo from './components/SlotDemo.vue';
-import SubmitButton from './components/submit-button.vue';
-import CurrentUser from './components/CurrentUser.vue';
-import ScopeSlot from './components/ScopeSlot.vue';
+import DynamicParam from './components/DynamicParam.vue';
 
 export default {
   name: 'App',
   components: {
-    TodoList,
-    NavigationLink,
-    MixinDemo,
-    SlotDemo,
-    SubmitButton,
-    CurrentUser,
-    ScopeSlot,
+    DynamicParam,
   },
   data() {
     return {
---
+++ src/components/DynamicParam.vue
@@ -0,0 +1,13 @@
+<template>
+  <a v-bind:[attributeName]="url">点击</a>
+</template>
+
+<script>
+export default {
+  data() {
+    return { attributeName: 'href', url: 'https://www.baidu.com' };
+  },
+};
+</script>
+
+<style></style>
